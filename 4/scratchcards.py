0f58ef518f839a2f6b3aba00f37da9973e109a58
MrDandas <daniel.stibor@gmail.com>
2023-12-04 16:33:45 +0100

    day 4 - part 2

--- 4/scratchcards.py
+++ 4/scratchcards.py
@@ -1,18 +1,21 @@
+from typing import Tuple, Dict, Set, Union, Any
+
 from util.part_enum import Part
 
 
 class Scratchcards(object):
 
     @staticmethod
-    def parse_line(*, line: str, part: Part) -> tuple[set[int], set[int]]:
+    def parse_line(*, line: str, part: Part) -> tuple[str, set[int], set[int]]:
         # Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        card_id = line.split(':')[0].split()[1]
         winning_numbers = set([int(s) for s in line.split(':')[1].split('|')[0].split() if s.isdigit()])
         my_numbers = set([int(s) for s in line.split(':')[1].split('|')[1].split() if s.isdigit()])
 
-        return winning_numbers, my_numbers
+        return card_id, winning_numbers, my_numbers
 
     @staticmethod
-    def solve(lines: list[str], *, part: Part = Part.ONE) -> tuple[set[int],set[int],set[int],int]:
+    def solve(lines: list[str], *, part: Part = Part.ONE):
         """
         For example:
 
@@ -32,23 +35,47 @@ class Scratchcards(object):
         Card 6 has no winning numbers, so it is worth no points.
         So, in this example, the Elf's pile of scratchcards is worth 13 points.
         """
+        card_registry = {}
+        cards_to_process = []
+        for line in lines:
+            card_id, winning, my_numbers = Scratchcards.parse_line(line=line, part=Part.ONE)
+            matches = winning & my_numbers
+            card_sum = len(matches) if len(matches) < 2 else pow(2, len(matches) - 1)
+            card_registry[int(card_id)] = (int(card_id), winning, my_numbers, matches, card_sum)
+        cards_to_process.extend(card_registry.values())
 
         if part == Part.ONE:
-            part_1_sum = 0
-            for line in lines:
-                winning, my_numbers = Scratchcards.parse_line(line=line, part=Part.ONE)
-                matches = winning & my_numbers
+            part_1_sum = sum([x[4] for x in cards_to_process])
+            return card_registry, part_1_sum
 
-                if len(matches) < 2:
-                    part_1_sum += len(matches)
-                else:
-                    part_1_sum += pow(2, len(matches) - 1)
+        if part == Part.TWO:
+            if len(cards_to_process) == 0:
+                return card_registry, 0
 
-            return (winning, my_numbers, matches, part_1_sum)
+            processed_count = 0
+            while processed_count < len(cards_to_process):
+                card = cards_to_process[processed_count]
+                # print(f'+ [{processed_count}] -- Processing card {card[0]}; [{len(card[3])}] matches')
+
+                # add new copies to the queue
+                if len(card[3]) > 0:
+                    # card no.1 is on index 0
+                    card_first = card[0] + 1
+                    card_last = min(card[0] + len(card[3]), len(card_registry))
+
+                    for i in range(card_first, card_last + 1):
+                        # print(f'+ [{processed_count}] -- Adding card {card_registry[i][0]}')
+                        cards_to_process.append(card_registry[i])
+
+                processed_count += 1
+
+            return processed_count
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     with open('input.txt') as file:
         input_lines = file.readlines()
 
-        print(f'Solution Part.ONE: {Scratchcards.solve(input_lines, part=Part.ONE)[3]}')
+        print(f'Solution Part.ONE: {Scratchcards.solve(input_lines, part=Part.ONE)[1]}')
+        print(f'Solution Part.TWO: {Scratchcards.solve(input_lines, part=Part.TWO)}')

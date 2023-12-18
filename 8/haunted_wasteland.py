e606e5356ff28ab4bcb6f5d2a5859f91f5dcbe91
MrDandas <daniel.stibor@gmail.com>
2023-12-18 22:05:12 +0100

    day 8 - part 1 -- refactor prep for part 2

--- 8/haunted_wasteland.py
+++ 8/haunted_wasteland.py
@@ -1,5 +1,6 @@
 import re
 from timeit import default_timer
+from collections.abc import Callable
 
 
 class HauntedWasteland(object):
@@ -10,34 +11,56 @@ class HauntedWasteland(object):
             yield from steps
 
     @staticmethod
-    def walk(directions: dict[str, tuple[str, str]], steps: list[int]):
+    def walk(directions: dict[str, tuple[str, str]], steps: list[int], starting_pos: str,
+             end_fn: Callable[[str], bool]):
 
         counter = 0
-        pos = 'AAA'
+        pos = starting_pos
         for i in HauntedWasteland.loop(steps):
-            print(f'[{counter}] : {pos} -> {directions[pos]} ---> {directions[pos][i]}')
-            if pos == 'ZZZ':
+            #print(f'[{counter}] : {pos} -> {directions[pos]} ---> {directions[pos][i]}')
+            if end_fn(pos):
                 break
             counter += 1
             pos = directions[pos][i]
         return counter
 
     @staticmethod
-    def solve(lines: list[str]):
-        steps = [1 if char == 'R' else 0 for char in lines[0].strip()]
-        directions = {}
+    def parse(lines: list[str]):
+        steps_ = [1 if char == 'R' else 0 for char in lines[0].strip()]
+        directions_ = {}
         for line in lines[1:]:
             if line[0].isalpha():
                 # AAA = (BBB, CCC)
-                beg, lhs, rhs = re.findall(r"[A-Z]+", line)
-                directions[beg] = (lhs, rhs)
+                beg, lhs, rhs = re.findall(r"[A-Z0-9]+", line)
+                directions_[beg] = (lhs, rhs)
 
-        return HauntedWasteland.walk(directions, steps)
+        return steps_, directions_
+
+    @staticmethod
+    def solve(lines: list[str]):
+        steps, directions = HauntedWasteland.parse(lines)
+
+        is_end: Callable[[str], bool] = lambda x: (x == 'ZZZ')
+
+        return HauntedWasteland.walk(directions, steps, 'AAA', is_end)
+
+    @staticmethod
+    def solve_2(lines: list[str]):
+        steps, directions = HauntedWasteland.parse(lines)
+        all_starts = [d for d in directions.keys() if d[2] == 'A']
+        is_end: Callable[[str], bool] = lambda x: (x[2] == 'Z')
+
+        start_to_cycle = {}
+        for s in all_starts:
+            start_to_cycle[s] = HauntedWasteland.walk(directions, steps, 'AAA', is_end)
+
+        return 42
 
 
 if __name__ == '__main__':
     with open('input.txt') as file:
         input_lines = file.readlines()
-
         start = default_timer()
+
         print(f'Solution PART1: {HauntedWasteland.solve(input_lines)}; took: {default_timer() - start} ms')
+        # print(f'Solution PART1: {HauntedWasteland.solve_2(input_lines)}; took: {default_timer() - start} ms')
